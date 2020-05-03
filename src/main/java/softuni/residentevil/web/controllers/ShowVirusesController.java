cbc387ebbbd001a74f9372f56545fe2236e0f93f
ilianachakarova <iliana1212@abv.bg>
2020-05-03 14:26:07 +0200

    work on front-end

--- src/main/java/softuni/residentevil/web/controllers/ShowVirusesController.java
+++ src/main/java/softuni/residentevil/web/controllers/ShowVirusesController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
+import softuni.residentevil.domain.models.view.CapitalViewModel;
 import softuni.residentevil.domain.models.view.VirusViewModel;
+import softuni.residentevil.service.CapitalService;
 import softuni.residentevil.service.VirusService;
 
 import java.util.stream.Collectors;
@@ -15,14 +17,17 @@ import java.util.stream.Collectors;
 public class ShowVirusesController extends BaseController {
     private final VirusService virusService;
     private final ModelMapper modelMapper;
+    private final CapitalService capitalService;
     @Autowired
-    public ShowVirusesController(VirusService virusService, ModelMapper modelMapper) {
+    public ShowVirusesController(VirusService virusService, ModelMapper modelMapper, CapitalService capitalService) {
         this.virusService = virusService;
         this.modelMapper = modelMapper;
+        this.capitalService = capitalService;
     }
 
     @GetMapping("/viruses/show")
-    public ModelAndView showViruses(ModelAndView modelAndView, @ModelAttribute(name = "viewModel") VirusViewModel virusViewModel) {
+    public ModelAndView showViruses(ModelAndView modelAndView, @ModelAttribute(name = "viewModel") VirusViewModel virusViewModel,
+                                    @ModelAttribute(name = "capitalsModel") CapitalViewModel capitalViewModel) {
         modelAndView.addObject("viewModel", virusViewModel);
         modelAndView.addObject("viruses",
                 this.virusService.findAllViruses().stream().map
@@ -31,6 +36,10 @@ public class ShowVirusesController extends BaseController {
                            virus.setReleasedOn(v.getReleasedOn().toString());
                              return virus;
                          }).collect(Collectors.toList()));
+        modelAndView.addObject("capitalsModel",capitalViewModel);
+        modelAndView.addObject("capitals",
+                this.capitalService.findAllCapitals().stream()
+        .map(c->this.modelMapper.map(c,CapitalViewModel.class)).collect(Collectors.toList()));
         return super.view("show-viruses",modelAndView);
     }
 }
